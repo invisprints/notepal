84b0a6c33b58f5f44e757148f4add7f6574e5c9e
invisprints <invisprints@outlook.com>
2023-12-11 01:19:50 +0800

    Update readwiseCSV.ts

--- templates/readwiseCSV.ts
+++ templates/readwiseCSV.ts
@@ -1,4 +1,11 @@
+function escapeCSVField(field) {
+  if (field && (field.includes(',') || field.includes('"') || field.includes('\n'))) {
+    return `"${field.replace(/"/g, '""')}"`;
+  }
+  return field;
+}
+
 export const readwiseCSV = `Highlight,Title,Author,URL,Note
 {% for note in notes %}
-{{note.highlight}},{{note.bookName}},{{note.author}}, , {{note.sideNote | trim }}
+{{ note.highlight | escapeCSV }},{{ note.bookName | escapeCSV }},{{ note.author | escapeCSV }}, , {{ note.sideNote | trim | escapeCSV }}
 {% endfor %}`;
